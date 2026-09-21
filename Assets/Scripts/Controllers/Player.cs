76a204bfb8702b3c0212dc2baf4ed9815b05d32f
Ptyraan <168381801+Ptyraan@users.noreply.github.com>
2026-09-21 18:56:11 -0400

    part 1 done

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
-    
+    public float bombTrailSpacing;
+    public int numberOfTrailBombs;
+
     // Update is called once per frame
     void Update()
     {
@@ -15,10 +17,22 @@ public class Player : MonoBehaviour
         {
             SpawnBombAtOffset(new Vector3(0, 1));
         }
+        if (Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs);
+        }
     }
 
     public void SpawnBombAtOffset(Vector3 inOffset)
     {
         Instantiate(bombPrefab, transform.position + inOffset, transform.rotation);
+    }
+
+    public void SpawnBombTrail(float inBombSpacing, int inNumberOfBombs)
+    { 
+        for (int i = 0; i < inNumberOfBombs; i++)
+        {
+            SpawnBombAtOffset(new Vector3(0, -(i + 1) * inBombSpacing));
+        }
     }
 }
